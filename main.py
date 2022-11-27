0ab57777fcfc09232001acc2a8f370e15a796357
Elazar <96201125+awakzdev@users.noreply.github.com>
2022-11-27 15:40:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ cp = CloudPath("s3://bucket-name/folder-path").download_to("")
 # Resize images by 50%
 for file in os.listdir('medical-images'):
     image = Image.open('medical-images/'+file)
-    image.resize((int(image.size[0] / 2), int(image.size[1] / 2))).save('medical-images/'{file}'.scaled50')
+    image.resize((int(image.size[0] / 2), int(image.size[1] / 2))).save('medical-images/scaled50-'+file)
 
 # Upload resized folder to 'medical-images-scaled'
 fs = s3fs.S3FileSystem().put("medical-images", "bucket-name/medical-images-scaled", recursive=True)
